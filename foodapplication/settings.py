a2b03375ca7d6144fd1d08415823f8b1c2277055
Balam Revanth <balamrevanth2004@gmail.com>
2025-05-14 16:30:55 +0530

    code in setting file

--- foodapplication/settings.py
+++ foodapplication/settings.py
@@ -88,6 +88,8 @@ DATABASES = {
     }
 }
 
+print("MYSQLHOST:", os.environ.get("MYSQLHOST"))
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
