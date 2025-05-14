dc7ccf162690ecdebf36b812457562d0de8a82b3
Balam Revanth <balamrevanth2004@gmail.com>
2025-05-14 15:46:10 +0530

    debug variable in settings file

--- foodapplication/settings.py
+++ foodapplication/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qvd@ywhrk-$^v0k4a$$l^-k6n5c9&(bb6v#ab&(lj9m4k5d4q8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
